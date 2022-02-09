75a453de4cd5b75e517e9ea6f1130f376b6dc70a
mr.Escow <escowxbox@gmail.com>
2022-02-10 02:07:15 +0300

    Create class DynamicObject

--- ToxiMan/GameObject.h
+++ ToxiMan/GameObject.h
@@ -1,4 +1,53 @@
+#pragma once
+#include "System.h"
+
 class IGameObject {
 public:
-	vitrual void Draw = 0;
+	virtual void Draw() = 0;
+	virtual void SetPosition(v2f position) = 0;
+};
+
+class GameObject :public IGameObject {
+	sf::Shape* pShape_ = nullptr;
+	// Shader
+	v2f m_direction{ 0,0 };
+	v2f m_position{ 0,0 };
+public:
+	GameObject(sf::Shape* shape) {
+		pShape_ = shape;
+	};
+	virtual void Draw() override {
+		if (pShape_)
+			System::wnd.draw(*pShape_/*Shader*/);
+	};
+	virtual void SetPosition(v2f position) override {
+		m_position = position;
+		if (pShape_)
+			pShape_->setPosition(m_position);
+	};
+	virtual ~GameObject() {
+		delete pShape_;
+	}
+
+};
+
+class DynamicObject :public GameObject {
+	float m_speed;
+	sf::Shape* pShape_;
+public:
+	DynamicObject(sf::Shape* shape):GameObject(shape){
+		pShape_ = shape;
+	};
+	void SetSpeed(float speed) {
+		m_speed = speed;
+	}
+	virtual void Move(v2f direction){
+		if (pShape_)
+			pShape_->move(direction * m_speed);
+	};
+};
+
+class BigSquare :public DynamicObject {
+public:
+	BigSquare(sf::RectangleShape* shape):DynamicObject(shape) {};
 };
